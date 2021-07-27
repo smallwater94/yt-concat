c6acd8a661b3f450713a2ab53ce445175db60155
York Wu <smallwater94@gmail.com>
2021-07-28 03:33:30 +0800

    Caption File organization

--- yt_concate/pipeline/steps/download_captions.py
+++ yt_concate/pipeline/steps/download_captions.py
@@ -1,6 +1,10 @@
 # 下載字幕
+import os
+
 from youtube_transcript_api import YouTubeTranscriptApi
 from yt_concate.pipeline.steps.step import Step, StepException
+
+from yt_concate.settings import CAPTION_DIR
 
 
 class DownloadCaptions(Step):
@@ -16,10 +20,13 @@ class DownloadCaptions(Step):
                 print('非英文或無字幕，網址: ', furl)
                 continue
 
-            with open(self.get_video_id_from_url(url) + '.txt', "w") as f:
+            with open(self.get_caption_path(url), "w") as f:
                 for i in srt:
                     f.write("{}\n".format(i))
 
     @staticmethod
     def get_video_id_from_url(url):
         return url.split('watch?v=')[-1]
+
+    def get_caption_path(self, url):
+        return os.path.join(CAPTION_DIR, self.get_video_id_from_url(url) + '.txt')
--- yt_concate/settings.py
+++ yt_concate/settings.py
@@ -4,3 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()  # take environment variables from .env.
 
 API_KEY = os.getenv('API_KEY')
+
+
+# 把CAP跟VID加進DOW裡面
+DOWNLOADS_DIR = 'downloads'  # 下載資料夾
+VIDEOS_DIR = os.path.join(DOWNLOADS_DIR, 'videos')  # 影片資料夾
+CAPTION_DIR = os.path.join(DOWNLOADS_DIR, 'captions')  # 字幕資料夾
