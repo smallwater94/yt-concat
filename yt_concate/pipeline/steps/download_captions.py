999e5d2888b4ccb9612d87a11616f55f2b0f5cb2
York Wu <smallwater94@gmail.com>
2021-07-28 02:37:17 +0800

    Update class download_captions.py

--- yt_concate/pipeline/steps/download_captions.py
+++ yt_concate/pipeline/steps/download_captions.py
@@ -1,20 +1,25 @@
 # 下載字幕
 from youtube_transcript_api import YouTubeTranscriptApi
-from yt_concate.pipeline.steps.step import Step
+from yt_concate.pipeline.steps.step import Step, StepException
 
 
 class DownloadCaptions(Step):
     def process(self, transporter, inputs):
-        for url in transporter:
-            url = url.split('watch?v=')[-1]
 
-            # 將字幕存進名為srt的字典
-            # 通過 .get_transcript() 函數獲得
-            srt = YouTubeTranscriptApi.get_transcript(url)
+        for furl in transporter:
+            try:
+                url = self.get_video_id_from_url(furl)  # 擷取影片網址代號的部分
 
-            for i in srt:
-                print(i)
+                # 使用 set 變量和 .get_transcript() 函數獲得的字典列表
+                srt = YouTubeTranscriptApi.get_transcript(url)
+            except:
+                print('非英文或無字幕，網址: ', furl)
+                continue
 
-            break
+            with open(self.get_video_id_from_url(url) + '.txt', "w") as f:
+                for i in srt:
+                    f.write("{}\n".format(i))
 
-
+    @staticmethod
+    def get_video_id_from_url(url):
+        return url.split('watch?v=')[-1]
